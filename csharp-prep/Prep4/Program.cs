fadce1575c3701fb6db35ac82a78b22900285683
seth-godoy <u443447760@byui.edu>
2023-04-26 17:28:28 -0600

    Finished Prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        // Display instructions.
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        
+        // Declare the necessary variables.
+        List<int> numbers = new List<int>();
+        string givenNumberText;
+        int givenNumber;
+
+        do
+        {
+            // Get a number from the user.
+            Console.Write("Enter number: ");
+            givenNumberText = Console.ReadLine();
+            givenNumber = int.Parse(givenNumberText);
+
+            // Add the number to a list.
+            numbers.Add(givenNumber);
+        } while (givenNumber != 0);
+
+        // Get the full sum of the numbers list.
+        int sum = 0;
+
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        // Get the average of the numbers list.
+        double average = (double)sum / ((double)numbers.Count - 1);
+
+        // Get the largest number.
+        int highestNumber = 0;
+
+        foreach (int number in numbers)
+        {
+            if (number > 0)
+            {
+                highestNumber = number;
+            }
+        }
+
+        // Display the results
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number: {highestNumber}");
     }
 }
