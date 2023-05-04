4c348b9d73dd189395241a958edff5e9d24af5b2
seth-godoy <u443447760@byui.edu>
2023-05-03 19:33:41 -0600

    Finished Learning02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,26 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Intel";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2015;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._company = "Google";
+        job2._jobTitle = "Project Manager";
+        job2._startYear = 2013;
+        job2._endYear = 2021;
+
+        Resume resume1 = new Resume();
+        resume1._name = "John Doe";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        resume1.DisplayResume();
     }
 }
