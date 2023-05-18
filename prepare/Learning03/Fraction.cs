142a14340565c460af53dfebeaa0d8f584753f3e
seth-godoy <u443447760@byui.edu>
2023-05-18 15:33:49 -0600

    Modified Fraction class

--- prepare/Learning03/Fraction.cs
+++ prepare/Learning03/Fraction.cs
@@ -2,5 +2,58 @@ using System;
 
 class Fraction
 {
-    
+    // ----- Attributes -----
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    // ----- Constructors -----
+    public Fraction(int top)
+    {
+        _top = top;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    // ----- Getters & Setters -----
+    public int GetTop()
+    {
+        return _top;
+    }
+
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+
+    // ----- Methods -----
+    public string GetFractionString()
+    {
+        return $"{_top}/{_bottom}";
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_top / _bottom;
+    }
 }
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction1 = new Fraction();
+        Fraction fraction2 = new Fraction(6);
+        Fraction fraction3 = new Fraction(6, 7);
+
+        fraction1.SetTop(2);
+        fraction1.SetBottom(4);
+        int numerator1 = fraction1.GetTop();
+        int denominator1 = fraction1.GetBottom();
+
+        fraction2.SetTop(10);
+        fraction2.SetBottom(5);
+        int numerator2 = fraction2.GetTop();
+        int denominator2 = fraction2.GetBottom();
+
+        fraction3.SetTop(1);
+        fraction3.SetBottom(3);
+        int numerator3 = fraction3.GetTop();
+        int denominator3 = fraction3.GetBottom();
+
+        // Fraction 1
+        Console.WriteLine(fraction1.GetFractionString());
+        Console.WriteLine(fraction1.GetDecimalValue() + "\n");
+
+        // Fraction 2
+        Console.WriteLine(fraction2.GetFractionString());
+        Console.WriteLine(fraction2.GetDecimalValue() + "\n");
+
+        // Fraction 3
+        Console.WriteLine(fraction3.GetFractionString());
+        Console.WriteLine(fraction3.GetDecimalValue() + "\n");
     }
 }
